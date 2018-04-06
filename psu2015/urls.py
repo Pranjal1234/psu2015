14687dcc6c5f668276cefb166e6b228cf034296f
Pranjal Patel <pranjalpatelpp@gmail.com>
2018-04-05 22:01:46 -0400

    Added Login and Logout templates

--- psu2015/urls.py
+++ psu2015/urls.py
@@ -20,5 +20,4 @@ urlpatterns = [
 	url(r'^$', include('home.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^home/', include('home.urls')),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
