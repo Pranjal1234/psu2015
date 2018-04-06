dabbdad679aa8bbae3487e1e1cf75b05f3d645f7
Pranjal Patel <pranjalpatelpp@gmail.com>
2018-04-05 20:02:44 -0400

    Added accounts to website

--- psu2015/urls.py
+++ psu2015/urls.py
@@ -13,11 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from registration.backends.simple.views import RegistrationView
 from django.conf.urls import url, include
 from django.contrib import admin
+from .forms import UserRegistrationForm
 
 urlpatterns = [
 	url(r'^$', include('home.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^home/', include('home.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'accounts/register/$', RegistrationView.as_view(
+        form_class=UserRegistrationForm
+    )),
 ]
