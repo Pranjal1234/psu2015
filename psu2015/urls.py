62a3032b167b05f5fcabe5f98f2d222b9fe9495a
Pranjal Patel <pranjalpatelpp@gmail.com>
2018-04-05 20:10:36 -0400

    Deleted the registration template

--- psu2015/urls.py
+++ psu2015/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from registration.backends.simple.views import RegistrationView
 from django.conf.urls import url, include
 from django.contrib import admin
 from .forms import UserRegistrationForm
@@ -23,7 +22,4 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^home/', include('home.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
-    url(r'accounts/register/$', RegistrationView.as_view(
-        form_class=UserRegistrationForm
-    )),
 ]
